8f3759d2995dab85ae2f759f88f895c4c78a5504
Eliashnf <eliashnf@gmail.com>
2023-03-02 19:46:58 +0100

    Home Page update

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,33 +2,107 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatefulWidget {
-  const HomePage({Key ? key}) : super(key: key);
+  const HomePage({Key? key}) : super(key: key);
 
   @override
   State<HomePage> createState() => _HomePageState();
 }
 
 class _HomePageState extends State<HomePage> {
-
   final user = FirebaseAuth.instance.currentUser!;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      body: SafeArea(
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text('Signed in as ' + user.email!),
-            MaterialButton(onPressed: (){
-              FirebaseAuth.instance.signOut();
-            },
-            color: Colors.green,
-            child: Text('Sign out'),
-            )
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Text(
+                'Signed in as ' + user.email!,
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+            Expanded(
+              child: Stack(
+                children: [
+                  Align(
+                    alignment: Alignment.center,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SizedBox(
+                          height: 80,
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              // Ajouter la fonctionalité d'ajout d'image
+                            },
+                            icon: Icon(Icons.add),
+                            label: Text('Upload your Image'),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 20),
+                        SizedBox(
+                          height: 80,
+                          width: double.infinity,
+                          child: ElevatedButton.icon(
+                            onPressed: () {
+                              // Ajouter la fonctionalité de prise de photo
+                            },
+                            icon: Icon(Icons.add),
+                            label: Text('Take a photo of your plant'),
+                            style: ElevatedButton.styleFrom(
+                              primary: Colors.green,
+                              elevation: 5,
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(10),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 16,
+                    left: 16,
+                    right: 16,
+                    child: SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          FirebaseAuth.instance.signOut();
+                        },
+                        child: Text('Sign out'),
+                        style: ElevatedButton.styleFrom(
+                          primary: Colors.red,
+                          elevation: 5,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ],
-        )
         ),
+      ),
     );
   }
 }
